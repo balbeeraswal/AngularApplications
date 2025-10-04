202103b1a92133365aa403238e106bebaf4ad638
balbeeraswal <itcell-swd-uk@nic.in>
2025-10-04 16:39:02 +0530

    ok

---
+++ src/app/layout/applicantLoginLayout/loginApplicant-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-layout',
+  templateUrl: './loginApplicant-layout.component.html',
+  styleUrls: ['./loginApplicant-layout.component.css']
+})
+export class loginApplicantLayoutComponent {
+
+}
